8dcac79cf14c3d616dfd2331dbd0a1b21315eb1e
oliverherrmann <o.herrmann92@gmail.com>
2017-11-06 20:00:17 +0100

    add first example templates

---
+++ assets/examples/c/file.js
@@ -0,0 +1,24 @@
+const os = require("os")
+
+module.exports = {
+    args: [
+        {name: "name", message: "The source file's name"},
+    ],
+    files: ({name}) => [
+        {
+            name: `${name}.c`,
+            content: `
+            
+            `
+        },
+        {
+            name: `${name}.h`,
+            content: `
+            #ifndef ${name.constCase()}_H
+            #define ${name.constCase()}_H
+            
+            #endif
+            `
+        }
+    ]
+}
